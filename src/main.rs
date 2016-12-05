2fdf0251f9fa785017a13ab0e9d42420e887b911
Emīls Piņķis <pinkisemils@gmail.com>
2016-12-05 11:44:47 +0000

    Ran rustfmt on main.rs

--- src/main.rs
+++ src/main.rs
@@ -23,20 +23,20 @@ fn par_fact_thread(lower: num::BigInt, upper: num::BigInt) -> BigInt {
     result
 }
 
-fn create_segments(upper_bound:u64, parts:u64) -> Vec<(u64, u64)> {
-    let mut segments: Vec<(u64,u64)> = vec![];
+fn create_segments(upper_bound: u64, parts: u64) -> Vec<(u64, u64)> {
+    let mut segments: Vec<(u64, u64)> = vec![];
 
-    let part:u64 = upper_bound/parts;
+    let part: u64 = upper_bound / parts;
     for i in 1..parts {
         let end = i * part;
-        segments.push((end-part+1, end));
+        segments.push((end - part + 1, end));
     }
 
-    let last_part:u64 = upper_bound % parts;
+    let last_part: u64 = upper_bound % parts;
     if last_part == 0 {
-        segments.push((upper_bound-part+1, upper_bound));
+        segments.push((upper_bound - part + 1, upper_bound));
     } else {
-        segments.push(((parts-1)*part+1, upper_bound));
+        segments.push(((parts - 1) * part + 1, upper_bound));
     }
     segments
 }
@@ -45,12 +45,12 @@ fn par_fact(n: u64, threads: u64) -> BigInt {
     let segments: Vec<(u64, u64)> = create_segments(n, threads);
 
     let threads: Vec<_> = segments.into_iter()
-                                 .map(|s| (s.0.to_bigint().unwrap(), s.1.to_bigint().unwrap())) 
-                                 .map(move |seg| thread::spawn(move || par_fact_thread(seg.0, seg.1)))
-                                 .collect();
+        .map(|s| (s.0.to_bigint().unwrap(), s.1.to_bigint().unwrap()))
+        .map(move |seg| thread::spawn(move || par_fact_thread(seg.0, seg.1)))
+        .collect();
     let inter_results: Vec<BigInt> = threads.into_iter()
-                                         .map(move |handle| handle.join().unwrap())
-                                         .collect::<Vec<_>>();
+        .map(move |handle| handle.join().unwrap())
+        .collect::<Vec<_>>();
     let mut result = BigInt::one();
     for i in inter_results.into_iter() {
         result = result * i;
@@ -61,7 +61,7 @@ fn par_fact(n: u64, threads: u64) -> BigInt {
 
 fn fact(n: u64) -> BigInt {
     let mut result = n.to_bigint().unwrap();
-    let mut i:BigInt = (n-1).to_bigint().unwrap();
+    let mut i: BigInt = (n - 1).to_bigint().unwrap();
     let one = BigInt::one();
     let mut iters = n;
     while iters > 1 {
@@ -71,18 +71,23 @@ fn fact(n: u64) -> BigInt {
     }
     result
 }
-fn time_fact(n:u64) {
+fn time_fact(n: u64) {
     let start = time::precise_time_ns();
     let result = fact(n);
     let end = time::precise_time_ns();
-    println!("Took {}ms\t to calculate {}! serially", (end-start)/1000000, n );
+    println!("Took {}ms\t to calculate {}! serially",
+             (end - start) / 1000000,
+             n);
 }
 
-fn time_par_fact(n:u64, threads:u64) {
+fn time_par_fact(n: u64, threads: u64) {
     let start = time::precise_time_ns();
     let result = par_fact(n, threads);
     let end = time::precise_time_ns();
-    println!("Took {}ms\t to calculate {}! concurrently with {} threads", (end-start)/1000000, n, threads);
+    println!("Took {}ms\t to calculate {}! concurrently with {} threads",
+             (end - start) / 1000000,
+             n,
+             threads);
 }
 
 fn main() {
